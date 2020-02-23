8e5546688dabd79b2cf6365450f387b3cdd03047
jiajiayao <jiajiayaoabc@qq.com>
2020-02-23 21:28:00 +0800

    前后端连通测试

--- flutter_website_service/data.dart
+++ flutter_website_service/data.dart
@@ -1,1 +1,101 @@
-//处理请求
+//模拟的数据
+var products={
+  "items":[
+    {
+      "desc":"屏幕尺寸 ：13.3寸 处理器：Intel Core i5-8295",
+      "imageUrl":"assets/image/product/1.jpeg",
+      "type":"新款",
+      "name":"苹果笔记本"
+    },
+    {
+      "desc":"屏幕尺寸 ：13.3寸 处理器：Intel Core i5-8295",
+      "imageUrl":"assets/image/product/1.jpeg",
+      "type":"新款",
+      "name":"苹果笔记本"
+    },
+    {
+      "desc":"屏幕尺寸 ：13.3寸 处理器：Intel Core i5-8295",
+      "imageUrl":"assets/image/product/1.jpeg",
+      "type":"新款",
+      "name":"苹果笔记本"
+    },
+    {
+      "desc":"屏幕尺寸 ：13.3寸 处理器：Intel Core i5-8295",
+      "imageUrl":"assets/image/product/1.jpeg",
+      "type":"新款",
+      "name":"苹果笔记本"
+    },
+    {
+      "desc":"屏幕尺寸 ：13.3寸 处理器：Intel Core i5-8295",
+      "imageUrl":"assets/image/product/1.jpeg",
+      "type":"新款",
+      "name":"苹果笔记本"
+    },
+    {
+      "desc":"屏幕尺寸 ：13.3寸 处理器：Intel Core i5-8295",
+      "imageUrl":"assets/image/product/1.jpeg",
+      "type":"新款",
+      "name":"苹果笔记本"
+    },
+    {
+      "desc":"屏幕尺寸 ：13.3寸 处理器：Intel Core i5-8295",
+      "imageUrl":"assets/image/product/1.jpeg",
+      "type":"新款",
+      "name":"苹果笔记本"
+    },
+    {
+      "desc":"屏幕尺寸 ：13.3寸 处理器：Intel Core i5-8295",
+      "imageUrl":"assets/image/product/1.jpeg",
+      "type":"新款",
+      "name":"苹果笔记本"
+    },
+    {
+      "desc":"屏幕尺寸 ：13.3寸 处理器：Intel Core i5-8295",
+      "imageUrl":"assets/image/product/1.jpeg",
+      "type":"新款",
+      "name":"苹果笔记本"
+    },
+  ]
+};
+
+var news = {
+  "items":[
+    {
+      "author":"佳佳",
+      "title":"添加商品",
+      "content":"小张添加了一个关于服务器大的商品"
+    },
+    {
+      "author":"佳佳",
+      "title":"添加商品",
+      "content":"小张添加了一个关于服务器大的商品"
+    },
+    {
+      "author":"佳佳",
+      "title":"添加商品",
+      "content":"小张添加了一个关于服务器大的商品"
+    },
+    {
+      "author":"佳佳",
+      "title":"添加商品",
+      "content":"小张添加了一个关于服务器大的商品"
+    },
+    {
+      "author":"佳佳",
+      "title":"添加商品",
+      "content":"小张添加了一个关于服务器大的商品"
+    },
+    {
+      "author":"佳佳",
+      "title":"添加商品",
+      "content":"小张添加了一个关于服务器大的商品"
+    },
+    {
+      "author":"佳佳",
+      "title":"添加商品",
+      "content":"小张添加了一个关于服务器大的商品"
+    },
+    
+
+  ]
+};
--- flutter_website_service/main.dart
+++ flutter_website_service/main.dart
@@ -1,2 +1,52 @@
-//模拟的数据
-import 'dart:convert';
+//处理请求
+import 'dart:convert';//json
+import 'dart:io';//http
+import 'data.dart';
+
+main() async {
+  var requestServer =await HttpServer.bind("192.168.3.57", 8080);
+  print('启动成功');
+  await for(HttpRequest request in requestServer){
+    handleMessage(request);
+  }
+}
+
+void handleMessage(HttpRequest request){
+  try{
+    if(request.method=='GET'){
+      handleGET(request);
+    }else if(request.method=='POST'){
+      handlePOST(request);
+    }
+  }catch(e){
+    print('捕获一个异常：$e');
+  }
+}
+
+void handleGET(HttpRequest request){
+
+  //获取请求参数
+  var action=request.uri.queryParameters['action'];
+  if(action == 'getProducts'){
+    print('获取产品数据。。。');
+    request.response
+    ..statusCode=HttpStatus.ok
+    ..write(json.encode(products))
+    ..close();
+  }
+  if(action == 'getNews'){
+    print('获取新闻数据。。。');
+    request.response
+    ..statusCode=HttpStatus.ok
+    ..write(json.encode(news))
+    ..close();
+  }
+
+  request.response..write('requset success..')..close();
+}
+
+void handlePOST(HttpRequest request){
+  //处理POST请求
+
+}
+
